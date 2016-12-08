0439b07c035d8c2b7ffb180831e62abe2b378dcd
wjmecho <wjmecho@gmail.com>
2016-12-08 23:43:05 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "include/JeuServeur.h"
+#include "../include/Piece.h"
+#include "../include/Pion.h"
+#include<vector>
+#include "../include/Joueur.h"
+#include "../include/Humain.h"
+
+using namespace std;
+
+
+
+
+int main()
+{
+    JeuServeur  Jeu;
+    Jeu.Jouer();
+
+}
